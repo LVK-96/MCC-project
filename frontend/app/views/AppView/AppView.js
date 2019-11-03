84712b3a0595cd3ea35cd6673e376f65361c5091
Lasse Arkko <lasse.arkko@aalto.fi>
2019-11-03 23:19:52 +0200

    Use AuthenticationProvider in AppView
    This provides all its children a valid AuthenticationContext value,
    allowing them to use authentication functionality and information.

--- frontend/app/views/AppView/AppView.js
+++ frontend/app/views/AppView/AppView.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import AuthenticationView from '../AuthenticationView';
+import AuthenticationProvider from '../../providers/AuthenticationProvider';
 
 /*A component that hosts everything application specific in the application
  (i.e. no framework-related setup code, but essentially everything else).*/
 function AppView() {
   return (
-    <AuthenticationView/>
+    <AuthenticationProvider>
+      <AuthenticationView/>
+    </AuthenticationProvider>
   );
 }
 
