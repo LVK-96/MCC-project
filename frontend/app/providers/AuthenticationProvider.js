bdff2c84ab9ae1a50bbd995125d177c85c58d3a2
Lasse Arkko <lasse.arkko@aalto.fi>
2019-11-03 23:28:24 +0200

    Store information on authentication in AuthenticationProvider
    This allows consumers of AuthenticationContext to check if the user is
    logged in and know things about the user they have logged in as.

--- frontend/app/providers/AuthenticationProvider.js
+++ frontend/app/providers/AuthenticationProvider.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AuthenticationContext from '../contexts/AuthenticationContext';
 
 /*Encapsulates authentication logic inside one component.*/
 function AuthenticationProvider({ children }) {
+  const [user, setUser] = useState(null);
+  /*We are logged in if the user is set (not null).*/
+  const isLoggedIn = !!user;
   const login = async ({ email, password }) => {
     if (email === 'placeholder@email.com' && password === 'password') {
+      setUser({ email, displayName: 'placeholder' });
       return {};
     } else {
       throw new Error("Failed to log in");
@@ -16,6 +20,7 @@ function AuthenticationProvider({ children }) {
       && password === 'password'
       && displayName === 'placeholder'
     ) {
+      setUser({ email, displayName });
       return {};
     } else {
       throw new Error("Failed to sign up");
@@ -24,6 +29,10 @@ function AuthenticationProvider({ children }) {
   const value = {
     login,
     signup,
+    user,
+    /*By providing this, we can avoid the reimplementation of the logic that
+      checks if a user is logged in outside of this component.*/
+    isLoggedIn,
   };
   return (
     <AuthenticationContext.Provider value={value}>
