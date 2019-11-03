9e7ad5bd7156ad01946aa68acbecc191d1d97890
Lasse Arkko <lasse.arkko@aalto.fi>
2019-11-03 22:08:41 +0200

    Add AuthenticationContext
    This context is for providing authentication related functionality (e.g.
    login and signup) and information (e.g. currently signed in user).

---
+++ frontend/app/contexts/AuthenticationContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+/*Context for providing authentication related functionality (e.g. login and
+  signup) and information (e.g. currently logged in user).*/
+const AuthenticationContext = React.createContext(null);
+
+export default AuthenticationContext;
