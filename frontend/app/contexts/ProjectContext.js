d1e418a3be60ff544b4746e32c1f4751d27e5361
Lasse Arkko <lasse.arkko@aalto.fi>
2019-11-05 22:04:31 +0200

    Add ProjectContext
    This context will provide project related functionality to the
    components.

---
+++ frontend/app/contexts/ProjectContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+/*Context for providing project related functionality (e.g. creation)
+  and information (e.g. list of projects).*/
+const ProjectContext = React.createContext(null);
+
+export default ProjectContext;
