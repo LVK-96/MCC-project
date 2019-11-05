d48c0a1b2d525a2a93fbf9847b75bb25d52f4f7b
Lasse Arkko <lasse.arkko@aalto.fi>
2019-11-06 00:17:35 +0200

    Add a ProjectList component
    This component shows a list of project previews.

---
+++ frontend/app/views/ProjectList/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  projectsContainer: {
+    flex: 1,
+  },
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
