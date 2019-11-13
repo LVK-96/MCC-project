1b3be0ec47b0ff18b026a54a2ab56fd3b2ea8b0d
jporvali <jporvali@gmail.com>
2019-11-13 15:02:09 +0200

    Add project form screen to project navigator

--- frontend/app/navigators/ProjectNavigator.js
+++ frontend/app/navigators/ProjectNavigator.js
@@ -4,11 +4,13 @@ import {
 import ProjectListNavigator from './ProjectListNavigator';
 import ProjectScreenNavigator from './ProjectScreenNavigator';
 import ProfileView from '../views/ProfileView/';
+import ProjectForm from '../views/ProjectFormView';
 
 /*Navigator for the various project related screens.*/
 export default createStackNavigator({
   ProjectList: ProjectListNavigator,
   Project: ProjectScreenNavigator,
+  ProjectForm: ProjectForm,
 }, {
   headerMode: 'none',
 });
