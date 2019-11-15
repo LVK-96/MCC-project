7d18b2e96af997d61e276db407ce73e801d0ca65
koskelj8 <jaakko.m.koskela@aalto.fi>
2019-11-15 23:30:51 +0200

    Implemented frontend compenents for mock login and signup
    Mock uid and token imported from mock.json in developement build to allow user verification in backend using firebase auth admin sdk. Also fixed rendering of displayname in userprofile.

--- frontend/app/providers/AuthenticationProvider.js
+++ frontend/app/providers/AuthenticationProvider.js
@@ -1,21 +1,22 @@
 import React, { useState } from 'react';
 import AuthenticationContext from '../contexts/AuthenticationContext';
-import auth, { firebase } from '@react-native-firebase/auth'
+import auth, { firebase } from '@react-native-firebase/auth';
+import mockData from '../../mock.json';
 
 /*Encapsulates authentication logic inside one component.*/
 function AuthenticationProvider({ children }) {
-  const [user, setUser] = useState({ email : 'nothing', displayName : 'nothing'});
+  const [user, setUser] = useState(null);
   /*We are logged in if the user is set (not null).*/
   const isLoggedIn = !!user;
   const login = async ({ email, password }) => {
     if (email === 'placeholder@email.com' && password === 'password') {
-      setUser({email: 'something', displayName: 'also something'});
+      setUser({email: 'placeholder@email.com', displayName: 'placeholder', uid: mockData.UID, token: mockData.TOKEN});
     } else {
       throw new Error('Failed to log in');
     }
   };
   const signup = async ({ email, displayName, password }) => {
-    if (false) {
+    if (__DEV__) {
       //developement build -  use mock data
       console.log('Developement');
       if (
@@ -23,7 +24,8 @@ function AuthenticationProvider({ children }) {
         && password === 'password'
         && displayName === 'placeholder'
       ) {
-        setUser({ email, displayName });
+        //ToDO: set uid and token into a state
+        setUser({email: email, displayName: displayName, uid: mockData.UID, token: mockData.TOKEN});
       } else {
         throw new Error('Failed to sign up with mock');
       }
@@ -31,12 +33,13 @@ function AuthenticationProvider({ children }) {
       //production build - use firebase auth
       console.log('Production');
       try {
-        console.log('waiting response form firebase auth');
+        console.log('waiting response from firebase auth');
         const temp = await auth().createUserWithEmailAndPassword(email, password);
         const temp2 = await firebase.auth().currentUser.getIdToken(true);
+        //ToDO: set uid and token into a state
+
         console.log(temp);
         console.log(temp2);
-        console.log('firebase auth responded successfully?');
       } catch (e) {
           console.log('failed to signup with firebase');
           throw new Error('Failed to signup with firebase auth');
