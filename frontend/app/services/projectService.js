ff3e7d3164d23e29f6a48b32eb85c0f86f8e3e47
jporvali <jporvali@gmail.com>
2019-11-15 17:44:29 +0200

    Remove redundant class

--- frontend/app/services/projectService.js
+++ frontend/app/services/projectService.js
@@ -3,33 +3,31 @@ import axios from 'axios';
 
 const baseUrl = 'http://10.0.2.2:3000/projects'; // TODO: use env var for this
 
-class ProjectService {
 
-	getAll = async () => {
-		try {
-			const response = await axios.get(baseUrl);
-			return response.data;
-		} catch (exception) {
-			return null;
-		}
-	};
-
-	createProject = async (project) => {
-		try {
-			console.log('Creating project', project.name);
-
-			// const response = await axios.post(baseUrl);
-			// return response.data;
-
-			// Remove these when back-end supports project creation.
-			project.creationDate = new Date();
-			project.id = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-			return project;
-		} catch (exception) {
-			console.log('Error creating project', exception);
-			throw exception;
-		}
+const getAll = async () => {
+	try {
+		const response = await axios.get(baseUrl);
+		return response.data;
+	} catch (exception) {
+		return null;
 	}
-}
+};
 
-export default new ProjectService();
+const createProject = async (project) => {
+	try {
+		console.log('Creating project', project.name);
+
+		// const response = await axios.post(baseUrl);
+		// return response.data;
+
+		// Remove these when back-end supports project creation.
+		project.creationDate = new Date();
+		project.id = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+		return project;
+	} catch (exception) {
+		console.log('Error creating project', exception);
+		throw exception;
+	}
+};
+
+export default { getAll, createProject };
