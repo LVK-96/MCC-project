3673897d5b5dede8e6d7497fd33f4719672dc375
koskelj8 <jaakko.m.koskela@aalto.fi>
2019-11-19 15:39:20 +0200

    Removed developement login and signup
    Removed developement login and signup and replaced it with shallow login
    with placeholder@email.com

--- frontend/app/providers/AuthenticationProvider.js
+++ frontend/app/providers/AuthenticationProvider.js
@@ -10,16 +10,10 @@ function AuthenticationProvider({ children }) {
   /*We are logged in if the user is set (not null).*/
   const isLoggedIn = !!user;
   const login = async ({ email, password }) => {
-    if (__DEV__) {
-      console.log('developement');
-      if (email === 'placeholder@email.com' && password === 'password') {
-        setUser({email: 'placeholder@email.com', displayName: 'placeholder', uid: mockData.UID, token: mockData.TOKEN});
-        projectService.setToken(mockData.TOKEN);
-      } else {
-        throw new Error('Failed to log in');
-      }
+    //TODO: remove this in production
+    if (email === 'placeholder@email.com'){
+      setUser({email: 'placeholder@email.com', uid: '13456' });
     } else {
-      console.log('production');
       const sign = await auth().signInWithEmailAndPassword(email, password);
       const token = await auth().currentUser.getIdToken(true); //does firebase track signed in users
       setUser({email: email, uid: sign.user.uid}); //how to retrieve displayName
@@ -27,37 +21,17 @@ function AuthenticationProvider({ children }) {
     }
   };
   const signup = async ({ email, displayName, password }) => {
-    if (__DEV__) {
-      //developement build -  use mock data
-      console.log('Developement');
-      if (
-        email === 'placeholder@email.com'
-        && password === 'password'
-        && displayName === 'placeholder'
-      ) {
-        //ToDO: set uid and token into a state
-        setUser({email: email, displayName: displayName, uid: mockData.UID});
-        projectService.setToken(mockData.TOKEN);
-      } else {
-        throw new Error('Failed to sign up with mock');
-      }
-    } else {
-      //production build - use firebase auth
-      console.log('Production');
-      try {
-        console.log('waiting response from firebase auth');
-        const sign = await auth().createUserWithEmailAndPassword(email, password);
-        const token = await firebase.auth().currentUser.getIdToken(true); //does firebase track signed in users - how does signing out work
-        setUser({email: email, displayName:displayName, uid: sign.user.uid});
-        projectService.setToken(token);
-
-      } catch (e) {
-          console.log('failed to signup with firebase');
-          throw new Error('Failed to signup with firebase auth');
-
-      }
-
-
+    //production build - use firebase auth
+    console.log('Production');
+    try {
+      console.log('waiting response from firebase auth');
+      const sign = await auth().createUserWithEmailAndPassword(email, password);
+      const token = await auth().currentUser.getIdToken(true); //does firebase track signed in users - how does signing out work
+      setUser({email: email, displayName:displayName, uid: sign.user.uid});
+      projectService.setToken(token);
+    } catch (e) {
+        console.log('failed to signup with firebase');
+        throw new Error('Failed to signup with firebase auth');
     }
   };
   const value = {
