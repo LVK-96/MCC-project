f7385da86806664d384d33d8d427f68e5d2c1b66
jporvali <jporvali@gmail.com>
2019-11-20 14:06:13 +0200

    Add initial tasks list view

---
+++ frontend/app/views/TaskList/TaskList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+    Text,
+} from 'react-native';
+
+function TaskList() {
+    return (
+        <Text>this is real tasks test</Text>
+    );
+}
+
+export default TaskList;
