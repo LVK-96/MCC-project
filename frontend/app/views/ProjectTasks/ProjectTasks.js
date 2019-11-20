e691bb8b27e48fb43fbf09a1bbb3eb5aa42ce908
jporvali <jporvali@gmail.com>
2019-11-20 15:09:05 +0200

    Add temporary project tasks container

--- frontend/app/views/ProjectTasks/ProjectTasks.js
+++ frontend/app/views/ProjectTasks/ProjectTasks.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import {
-  Text
-} from 'react-native';
+import React, { useContext } from 'react';
+import ProjectContext from '../../contexts/ProjectContext';
+import TasksProvider from '../../providers/TasksProvider';
+import TasksNavigator from '../../navigators/TasksNavigator';
 
-function ProjectTasks() {
+function ProjectTasks({ navigation }) {
+  const { selectedProject } = useContext(ProjectContext);
+
   return (
-    <Text>
-      Project tasks placeholder
-    </Text>
+    <TasksProvider projectId={selectedProject.id}>
+      <TasksNavigator navigation={navigation}/>
+    </TasksProvider>
   );
 }
+
+ProjectTasks.router = TasksNavigator.router;
 
 export default ProjectTasks;
