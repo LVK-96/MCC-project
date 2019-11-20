40a4d33fe9e3c4284e75a71de1c3f51ef39b3774
jporvali <jporvali@gmail.com>
2019-11-20 15:36:40 +0200

    Add initial styles for task list

---
+++ frontend/app/views/TaskList/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  tasksContainer: {
+    flex: 1,
+  },
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
