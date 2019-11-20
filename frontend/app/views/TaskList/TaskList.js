f0486feb89ddd6c027fcfeae54c997d7b7d56e82
jporvali <jporvali@gmail.com>
2019-11-20 15:37:14 +0200

    Update task list

--- frontend/app/views/TaskList/TaskList.js
+++ frontend/app/views/TaskList/TaskList.js
@@ -2,21 +2,51 @@ import React, { useContext } from 'react';
 import {
     Text,
     View,
+    ScrollView,
+    ActivityIndicator,
+    TouchableOpacity,
 } from 'react-native';
 import TasksContext from '../../contexts/TasksContext';
+import styles from './styles';
+import TaskPreview from '../TaskPreview';
+import colors from '../../values/colors';
+import PlusIcon from '../PlusIcon';
 
-function TaskList() {
-    const tasksContext = useContext(TasksContext);
+function TaskList({ navigation }) {
+    const { tasks } = useContext(TasksContext);
 
-    return (
-        <View>
-            {tasksContext.tasks.map(task =>
-                <View key={task.id}>
-                    <Text>{task.description}</Text>
-                </View>
-            )}
+    const viewTask = (taskId) => {
+
+    };
+
+    const contentArea = tasks ? (
+        <ScrollView style={styles.tasksContainer}>
+          {tasks.map(task =>
+            <TaskPreview
+              key={task.id}
+              onPress={() => viewTask(task.id)}
+              task={task}
+            />
+          )}
+        </ScrollView>
+      ) : (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color={colors.corporateBlue}/>
         </View>
-    );
+      );
+    
+      return (
+        <View style={styles.container}>
+          <Text>
+            Project tasks
+          </Text>
+          {contentArea}
+          <TouchableOpacity onPress={() => navigation.navigate('ProjectForm')}
+            style={styles.createProjectButtonContainer}>
+            <PlusIcon />
+          </TouchableOpacity>
+        </View>
+      );
 }
 
 export default TaskList;
