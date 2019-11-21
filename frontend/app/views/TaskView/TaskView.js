9c414b18354d1517b5914788d7b51e66349fc3d2
jporvali <jporvali@gmail.com>
2019-11-21 16:57:38 +0200

    Add initial styles for task view

--- frontend/app/views/TaskView/TaskView.js
+++ frontend/app/views/TaskView/TaskView.js
@@ -8,6 +8,7 @@ import {
 import ProjectContext from '../../contexts/ProjectContext';
 import TasksContext from '../../contexts/TasksContext';
 import { withNavigation } from 'react-navigation';
+import styles from './styles';
 
 function TaskView() {
     const { selectedProject } = useContext(ProjectContext);
@@ -20,25 +21,27 @@ function TaskView() {
     }
 
     return (
-        <View>
-            <View>
-                <Text>Task description</Text>
-                <TextInput value={task.description}/>
+        <View style={styles.container}>
+            <View style={styles.innerContainer}>
+                <View>
+                    <Text style={styles.label}>Task description</Text>
+                    <TextInput value={task.description}/>
+                </View>
+                <View>
+                    <Text style={styles.label}>Task status</Text>
+                    <TextInput value={task.status}/>
+                </View>
+                <View>
+                    <Text style={styles.label}>Task deadline</Text>
+                    <TextInput value={task.deadline}/>
+                </View>
+                {/* We only render the assignees selection if the project
+                this task is associated with is a group project. */
+                selectedProject.type === 'GROUP' &&
+                <View>
+                    <Text style={styles.label}>Task assignees</Text>
+                </View>}
             </View>
-            <View>
-                <Text>Task status</Text>
-                <TextInput value={task.status}/>
-            </View>
-            <View>
-                <Text>Task deadline</Text>
-                <TextInput value={task.deadline}/>
-            </View>
-            {/* We only render the assignees selection if the project
-            this task is associated with is a group project. */
-            selectedProject.type === 'GROUP' &&
-            <View>
-                <Text>Task assignees</Text>
-            </View>}
         </View>
     );
 }
