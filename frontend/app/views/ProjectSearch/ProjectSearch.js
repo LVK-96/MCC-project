7345a66b962b5a19d7faa40fb3611a365deffb66
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2019-11-23 19:02:49 +0200

    Search with keyword list

--- frontend/app/views/ProjectSearch/ProjectSearch.js
+++ frontend/app/views/ProjectSearch/ProjectSearch.js
@@ -1,35 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import {
   View,
-  Switch,
-  Text,
+  Picker,
   TextInput,
 } from 'react-native';
 import styles from './styles';
+import ProjectContext from '../../contexts/ProjectContext';
 import ProjectList from '../ProjectList';
 
+function SearchField({ isTextInput, searchParam, setSearchParam }) {
+  const projectContext = useContext(ProjectContext);
+
+  if (isTextInput) {
+    return (
+      <TextInput style={styles.searchInput} value={searchParam} onChangeText={value => setSearchParam(value)}/>
+    );
+  } else {
+    const keywords = [ ...new Set(projectContext.projects.map(p => p.keywords).flat()) ];
+    return (
+      <Picker style={styles.keywordPicker} mode="dropdown"
+        value={searchParam}
+        onValueChange={value => setSearchParam(value)}>
+        {keywords.map(k =>
+          <Picker.Item label={k} value={k} key={keywords.indexOf(k)} />
+        )}
+      </Picker>
+    );
+  }
+}
+
 function ProjectSearch() {
-  const [switchValue, setSwitchValue] = useState(false);
+  const [switchValue, setSwitchValue] = useState('name');
   const [searchCriteria, setSearchCriteria] = useState('name');
   const [searchParam, setSearchParam] = useState('');
 
-  const toggleSwitch = () => {
-    setSwitchValue(!switchValue);
-    if (!switchValue) {
-      setSearchCriteria('keyword');
-    } else {
-      setSearchCriteria('name');
+  function handleSwitch(value) {
+    setSwitchValue(value);
+    switch (value) {
+      case 'keyword_list':
+        setSearchCriteria('keyword');
+        break;
+
+      case 'keyword':
+        setSearchCriteria('keyword');
+        break;
+
+      case 'name':
+        setSearchCriteria('name');
+        break;
+
+      default:
+        setSearchCriteria('name');
     }
-  };
+  }
 
   return (
     <View style={styles.container} >
+      <View style={styles.pickerContainer}>
+        <Picker style={styles.searchPicker}
+          selectedValue={switchValue}
+          onValueChange={value => handleSwitch(value)}>
+          <Picker.Item label="Name" value="name" />
+          <Picker.Item label="Keyword" value="keyword" />
+          <Picker.Item label="Keyword list" value="keyword_list" />
+        </Picker>
+      </View>
       <View style={styles.searchContainer}>
-        <TextInput style={styles.searchInput} value={searchParam} onChangeText={value => setSearchParam(value)}/>
-        <View style={styles.toggleContainer}>
-          <Switch style={styles.searchToggle} value={switchValue} onValueChange={toggleSwitch} />
-          <Text>{searchCriteria}</Text>
-        </View>
+        <SearchField isTextInput={switchValue !== 'keyword_list'} searchParam={searchParam} setSearchParam={setSearchParam} />
       </View>
       <View style={styles.projectContainer}>
         <ProjectList filter={searchCriteria} searchParam={searchParam} />
--- frontend/app/views/ProjectSearch/styles.js
+++ frontend/app/views/ProjectSearch/styles.js
@@ -9,25 +9,34 @@ export default StyleSheet.create({
     height: '100%',
   },
   searchContainer: {
+    alignSelf: 'center',
+    width: '100%',
     flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     marginHorizontal: 20,
     marginVertical: 20,
   },
-  searchInput: {
-    height: 40,
-    width: '70%',
-    backgroundColor: 'lightgrey'
+  searchPicker: {
+    width: '80%'
   },
-  toggleContainer: {
+  pickerContainer: {
     flex: 1,
-    alignSelf: 'flex-start',
-    flexDirection: 'column',
-    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    width: '100%',
+    height: '20%'
   },
   projectContainer: {
     marginVertical: 20,
-    height: '90%'
+    height: '80%'
+  },
+  searchInput: {
+    height: 40,
+    width: '80%',
+    backgroundColor: 'lightgrey'
+  },
+  keywordPicker: {
+    width: '80%',
   }
 });
