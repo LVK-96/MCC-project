27540b34a00277213ac5ce38faf31af5bb1959da
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2019-12-04 12:50:34 +0200

    Emulator sees localhost at 10.0.2.2

--- frontend/app/util/config.js
+++ frontend/app/util/config.js
@@ -1,5 +1,5 @@
 console.log(__DEV__);
-let api_url = 'http://localhost:3000/'
+let api_url = 'http://10.0.2.2:3000/'
 if (false) { // TODO: This is the production config
   api_url = 'https://mcc-fall-2019-g20.appspot.com/';
 }
