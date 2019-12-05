2364a0cea455c825f3c67fdc82944199f28abf68
jporvali <jporvali@gmail.com>
2019-12-05 17:54:43 +0200

    Add file downloading logic to project files view

--- frontend/app/views/ProjectFiles/ProjectFiles.js
+++ frontend/app/views/ProjectFiles/ProjectFiles.js
@@ -1,10 +1,11 @@
 import React, { useContext, useState, useEffect } from 'react';
 import {
-  View,
-  Text,
-  ActivityIndicator,
-  ScrollView,
-  TouchableOpacity,
+	View,
+	Text,
+	ActivityIndicator,
+	ScrollView,
+	TouchableOpacity,
+	Alert,
 } from 'react-native';
 import ProjectContext from '../../contexts/ProjectContext';
 import PlusIcon from '../PlusIcon';
@@ -12,76 +13,88 @@ import colors from '../../values/colors';
 import projectService from '../../services/projectService';
 import styles from './styles';
 import FilePickerManager from 'react-native-file-picker';
+import storage from '@react-native-firebase/storage';
+import RNFetchBlob from 'rn-fetch-blob';
 
 function ProjectFiles() {
-  const { selectedProject, addFile } = useContext(ProjectContext);
-  const [files, setFiles] = useState([]);
+	const { selectedProject, addFile } = useContext(ProjectContext);
+	const [files, setFiles] = useState([]);
 
-  // Re-fetch the files when the selected project changes.
-  useEffect(() => {
+	// Re-fetch the files when the selected project changes.
+	useEffect(() => {
 
-    async function fetchFiles(id) {
-      const fetched = await projectService.getFilesByProjectId(id);
-      setFiles(fetched);
-    }
+		async function fetchFiles(id) {
+			const fetched = await projectService.getFilesByProjectId(id);
+			setFiles(fetched);
+		}
 
-    if (selectedProject) {
-      fetchFiles(selectedProject.id);
-    }
+		if (selectedProject) {
+			fetchFiles(selectedProject.id);
+		}
 
-  }, [selectedProject]);
+	}, [selectedProject]);
 
-  const handleUploadFile = () => {
-    FilePickerManager.showFilePicker(null, res => {
-      if (res.didCancel || res.error) {
-        console.log('Failed to pick file');
-        return;
-      }
+	const handleFileUpload = () => {
+		FilePickerManager.showFilePicker(null, res => {
+			if (res.didCancel || res.error) {
+				console.log('Failed to pick file');
+				return;
+			}
 
-      const file = {
-        name: res.fileName,
-        uri: res.uri,
-      };
+			const file = {
+				name: res.fileName,
+				uri: res.uri,
+			};
 
-      const ret = addFile(selectedProject, file);
-      // TODO: Don't if this wasn't successful.
-      if (ret || !ret) {
-        // TODO: Use real response object.
-        setFiles(prev => [...prev, file]);
-      }
-    });
-  };
+			const ret = addFile(selectedProject, file);
+			// TODO: Don't if this wasn't successful.
+			if (ret || !ret) {
+				// TODO: Use real response object.
+				setFiles(prev => [...prev, file]);
+			}
+		});
+	};
 
-  const contentArea = selectedProject ? (
-    <ScrollView >
-      {files.map(file =>
-      // TODO: Only use uuid as key.
-      <View key={file.uuid || file.uri}
-        style={styles.file}>
-        <Text>
-          {file.name}
-        </Text>
-      </View>
-      )}
-    </ScrollView>
-  ) : (
-    <View >
-      <ActivityIndicator size="large" color={colors.corporateBlue}/>
-    </View>
-  );
+	const handleFileDownload = async (file) => {
+		try {
+			const storageRef = storage().ref();
+			const url = await storageRef.child(file.source).getDownloadURL();
+			const dirs = RNFetchBlob.fs.dirs;
+			RNFetchBlob.config({ path: dirs.DocumentDir })
+				.fetch('GET', url);
+		} catch (exception) {
+			Alert.alert('Failed to download file ' + file.name);
+		}
+	};
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>
-        Project files
+	const contentArea = selectedProject ? (
+		<ScrollView >
+			{files.map(file =>
+				// TODO: Only use uuid as key.
+				<View key={file.uuid || file.uri}
+					style={styles.file}>
+					<TouchableOpacity onPress={() => handleFileDownload(file)}>
+						<Text>{file.name}</Text>
+					</TouchableOpacity>
+				</View>
+			)}
+		</ScrollView>
+	) : (<View >
+			<ActivityIndicator size="large" color={colors.corporateBlue} />
+		</View>);
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.header}>
+				Project files
       </Text>
-      {contentArea}
-      <TouchableOpacity onPress={handleUploadFile}
-        style={styles.buttonContainer}>
-        <PlusIcon />
-      </TouchableOpacity>
-    </View>
-  );
+			{contentArea}
+			<TouchableOpacity onPress={handleFileUpload}
+				style={styles.buttonContainer}>
+				<PlusIcon />
+			</TouchableOpacity>
+		</View>
+	);
 }
 
 export default ProjectFiles;
