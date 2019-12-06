45af6fa38262aabdced6e10056b573534a25766f
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2019-12-06 11:44:36 +0200

    Correct async/await for notifications

--- functions/index.js
+++ functions/index.js
@@ -30,36 +30,57 @@ if (DEVELOPMENT) {
 //
 
 exports.deadline = functions.region('europe-west1')
-  .pubsub.schedule('* * * * *') // Check deadlines every hour
+  .pubsub.schedule('* * * * *') // Check deadlines every minute
   .onRun(async (context) => {
-    deadlineCheck();
+    try {
+      await deadlineCheck();
+    } catch (e) {
+      console.log('Deadline check failed')
+    }
 });
 
 exports.projectCreated = functions.region('europe-west1')
   .firestore
   .document('projects/{projectId}')
-  .onCreate((snap, context) => {
-  projectCreated(snap.data());
+  .onCreate(async (snap, context) => {
+    try {
+      await projectCreated(snap.data());
+    } catch (e) {
+      console.log('Project created notification failed')
+    }
 });
 
 exports.memberAdded = functions.region('europe-west1')
   .firestore
   .document('projects/{projectId}/members/{userId}')
   .onCreate(async (snap, context) => {
-    const doc = await admin.firestore().collection('projects').doc(projectId).get();
-    const project = doc.data();
-    memberAdded(snap.data(), project);
+    try {
+      const doc = await admin.firestore().collection('projects').doc(projectId).get();
+      const project = doc.data();
+      await memberAdded(snap.data(), project);
+    } catch (e) {
+      console.log('Member added notification failed');
+    }
 });
 
 exports.taskAssigned = functions.region('europe-west1')
   .firestore
-  .document('projects/{projectId}/tasks/{taskId}')
+  .document('projects/{projectId}/tasks/{taskId}/asignees/{asigneeId}')
   .onCreate(async (snap, context) => {
-    const doc = await admin.firestore().collection('projects').doc(projectId).get();
-    const project = doc.data();
-    taskAssigned(snap.data());
+    try {
+      const doc = await admin.firestore().collection('projects').doc(projectId)
+        .collection('tasks').doc(taskId).get();
+      const task = doc.data();
+      await taskAssigned(snap.data(), task);
+    } catch (e) {
+      console.log('Task assigned notification failed');
+    }
 });
 
-exports.generatePDF = functions.region('europe-west1').https.onRequest((request, response) => {
-  generatePDF(request, response);
+exports.generatePDF = functions.region('europe-west1').https.onRequest(async (request, response) => {
+  try {
+    await generatePDF(request, response);
+  } catch (e) {
+    console.log('PDF generation failed');
+  }
 });
