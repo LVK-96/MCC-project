61ee29edc0ecab4f7346bc8c3d7f56655215dc87
Lasse Arkko <lasse.arkko@aalto.fi>
2019-12-08 19:27:07 +0200

    Add comment to ProjectMembers

--- frontend/app/views/ProjectMembers/ProjectMembers.js
+++ frontend/app/views/ProjectMembers/ProjectMembers.js
@@ -3,6 +3,8 @@ import MembersList from '../MembersList';
 import MembersProvider from '../../providers/MembersProvider';
 import ProjectContext from '../../contexts/ProjectContext';
 
+/*A component for displaying the members of the current project.
+  Wraps around MembersList by providing it the members via a MembersProvider.*/
 const ProjectMembers = () => {
   const { selectedProject } = useContext(ProjectContext);
 
